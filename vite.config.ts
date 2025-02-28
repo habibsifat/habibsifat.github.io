c2439f0e6716d9f4d606a1add035a8130d058511
Md Habibur Rahman Sifat <habibsifatuiu@gmail.com>
2025-02-28 17:05:51 +0800

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: './', // This ensures that the built assets use relative paths, which is important for GitHub Pages
-})
+  base: '/habibsifat.github.io/', // Set the base URL for GitHub Pages
+});
